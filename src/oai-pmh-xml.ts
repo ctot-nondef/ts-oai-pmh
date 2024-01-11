747905f122e7e97470cb803ec136d7908a8185c3
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-01-11 22:24:37 +0100

    return specific response types from XML parser

--- src/oai-pmh-xml.ts
+++ src/oai-pmh-xml.ts
@@ -1,15 +1,25 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import * as xml2js from "xml2js";
 
+import type { TOAIListVerbs, TOAISingleVerbs} from "./EOAIVerbs.enum";
 import { OaiPmhError } from "./errors";
-import * as OAIResponseTypes from "./TOAIResponse.type"
+import {
+	type TOAIGetRecordsResponse,
+	type TOAIIdentifyResponse,
+	type TOAIListIdenfifiersResponse, type TOAIListMetadataFormatsResponse,
+	type TOAIListRecordsResponse,
+	type TOAIListSetsResponse, ZOAIResponse
+} from "./TOAIResponse.type";
+
 
 /**
  * parses an XML response into JSON and checks for the availability of basic properties
  * required for a valid OAI-PMH response
  * @param xml
+ * @param verb
  * @returns {Promise<any>}
  */
-export async function parseOaiPmhXml(xml: string): Promise<OAIResponseTypes.TOAIResponse> {
+export async function parseOaiPmhXml(xml: string, verb: TOAIListVerbs | TOAISingleVerbs): Promise<TOAIGetRecordsResponse | TOAIIdentifyResponse | TOAIListIdenfifiersResponse | TOAIListMetadataFormatsResponse | TOAIListRecordsResponse | TOAIListSetsResponse> {
 	const parser = new xml2js.Parser({
 		explicitCharkey: true,
 		explicitArray: true,
@@ -17,7 +27,7 @@ export async function parseOaiPmhXml(xml: string): Promise<OAIResponseTypes.TOAI
 		normalize: true,
 	});
 	const obj = (await parser.parseStringPromise(xml)) as Record<string, string>;
-	const oaiPmh = OAIResponseTypes.ZOAIResponse.passthrough().parse(obj);
+	const oaiPmh = ZOAIResponse.passthrough().parse(obj);
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 	if (!oaiPmh) {
 		throw new OaiPmhError("Returned data does not conform to OAI-PMH", "none");
@@ -30,5 +40,18 @@ export async function parseOaiPmhXml(xml: string): Promise<OAIResponseTypes.TOAI
 		});
 	}
 
-	return oaiPmh;
+	switch (verb) {
+		case "GetRecord":
+			return oaiPmh["OAI-PMH"].GetRecord!;
+		case "Identify":
+			return oaiPmh["OAI-PMH"].Identify!;
+		case "ListIdentifiers":
+			return oaiPmh["OAI-PMH"].ListIdentifiers!;
+		case "ListMetadataFormats":
+			return oaiPmh["OAI-PMH"].ListMetadataFormats!;
+		case "ListRecords":
+			return oaiPmh["OAI-PMH"].ListRecords!;
+		case "ListSets":
+			return oaiPmh["OAI-PMH"].ListSets!;
+	}
 }
